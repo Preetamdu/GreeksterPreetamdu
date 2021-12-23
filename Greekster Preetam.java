06a81ca98b7468fc2b9387ee87dd23e5b4eb924f
Preetamdu <96307782+Preetamdu@users.noreply.github.com>
2021-12-23 15:03:12 +0530

    initial commit

---
+++ Greekster Preetam.java
@@ -0,0 +1,14 @@
+public class MyClass {
+    public static void main(String args[])
+    {
+      int x=10;
+      int y=25;
+      int z=5;
+      if(x>=y && x>=z)
+     System.out.println(x+"is the greatest Number");
+     else if(y>=x && y>=z)
+     System.out.println(y+"is the greatest Number");
+     else
+     System.out.println(z+"is the greatest Number");
+    }
+}
